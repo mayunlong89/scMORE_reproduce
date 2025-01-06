1adc01507f218cac92fbc2e41075c0282287e132
Yunlong Ma <11418032@zju.edu.cn>
2025-01-05 20:35:18 -0500

    Update Figure_2A.r

--- Figures/Figure_2A.r
+++ Figures/Figure_2A.r
@@ -1,9 +1,6 @@
 
-
 # Load necessary libraries
 library(Seurat)
-library(ggplot2)
-library(patchwork) # For combining plots
 #---load pbmc_10x_real_data_downsampled_2000
 pbmc_10x_real_data_downsampled_2000 <- readRDS("/Users/mayunlong/Desktop/WMU2024/ctDRTF-codes and data/scHBO/10X_PBMC/10X_PBMC_downsample_2000cells.rds")
 
