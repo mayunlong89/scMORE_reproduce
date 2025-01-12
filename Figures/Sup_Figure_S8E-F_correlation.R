f8c3fb500ce073238e52654e82b4e750aa83fdc8
Yunlong Ma <11418032@zju.edu.cn>
2025-01-12 10:43:51 -0500

    Create Sup_Figure_S8E-F_correlation.R

---
+++ Figures/Sup_Figure_S8E-F_correlation.R
@@ -0,0 +1,71 @@
+##------glm_glmnet_xgb data transformed for correlation------------------
+
+library(reshape2)
+
+consine_average10traits_heatmap_glm <- read.table("01_10blood_cell_traits_sig_heatmap.txt",header = TRUE,sep = "\t")
+
+consine_average10traits_heatmap_xgb <- read.table("01_10blood_cell_traits_sig_heatmap_xgb.txt",header = TRUE,sep = "\t")
+
+consine_average10traits_heatmap_glmnet <- read.table("01_10blood_cell_traits_sig_heatmap_glmnet.txt",header = TRUE,sep = "\t")
+
+
+md_glm <- melt(consine_average10traits_heatmap_glm, id=c("regulons"))
+md_xgb <- melt(consine_average10traits_heatmap_xgb, id=c("regulons"))
+md_glmnet <- melt(consine_average10traits_heatmap_glmnet, id=c("regulons"))
+
+data <- md_glm
+
+colnames(data) <- c("regulons","traits","glm_TRS")
+head(data)
+data$xgb_TRS <- md_xgb$value
+data$glmnet_TRS <- md_glmnet$value
+
+
+cor.test(data$glm_TRS,data$xgb_TRS)
+cor.test(data$glm_TRS,data$glmnet_TRS)
+cor.test(data$glmnet_TRS,data$xgb_TRS)
+
+
+#Figure S8E----correlation glm_TRS VS. xgb_TRS
+ggplot(data, aes(x = glm_TRS, y = xgb_TRS,color=traits)) +
+  geom_point(size = 2, alpha = 0.7) +  # 散点
+  geom_smooth(method = "lm", se = FALSE, linetype = "dashed") +  # 添加拟合线
+  labs(title = "Correlation by Cell Type",
+       x = "glm_TRS",
+       y = "xgb_TRS") +
+  theme_classic()  # 经典主题
+
+# Perform correlation test for each cell type
+cor_results <- data %>%
+  dplyr::group_by(traits) %>%
+  dplyr::summarise(
+    correlation = cor(glm_TRS, xgb_TRS, use = "complete.obs"),  # Pearson correlation
+    p_value = cor.test(glm_TRS, xgb_TRS)$p.value,  # Extract p-value
+    conf_low = cor.test(glm_TRS, xgb_TRS)$conf.int[1],  # Lower bound of confidence interval
+    conf_high = cor.test(glm_TRS, xgb_TRS)$conf.int[2]   # Upper bound of confidence interval
+  )
+
+print(cor_results)
+
+
+#Figure S8F---correlation glm_TRS VS. glmnet_TRS
+ggplot(data, aes(x = glm_TRS, y = glmnet_TRS,color=traits)) +
+  geom_point(size = 2, alpha = 0.7) +  # 散点
+  geom_smooth(method = "lm", se = FALSE, linetype = "dashed") +  # 添加拟合线
+  labs(title = "Correlation by Cell Type",
+       x = "glm_TRS",
+       y = "glmnet_TRS") +
+  theme_classic()  # 经典主题
+
+
+# Perform correlation test for each cell type
+cor_results <- data %>%
+  dplyr::group_by(traits) %>%
+  dplyr::summarise(
+    correlation = cor(glm_TRS, glmnet_TRS, use = "complete.obs"),  # Pearson correlation
+    p_value = cor.test(glm_TRS, glmnet_TRS)$p.value,  # Extract p-value
+    conf_low = cor.test(glm_TRS, glmnet_TRS)$conf.int[1],  # Lower bound of confidence interval
+    conf_high = cor.test(glm_TRS, glmnet_TRS)$conf.int[2]   # Upper bound of confidence interval
+  )
+
+print(cor_results)
