b6f00f5d1c1717b7c6eefe2352974a6bdde81a81
Yunlong Ma <11418032@zju.edu.cn>
2025-09-22 11:28:33 -0400

    Update Figure_7A.R

--- Figures/Figure_7A.R
+++ Figures/Figure_7A.R
@@ -1,1 +1,2 @@
 This is a workflow of how to use scHOB for scMORE-based integrative analysis based on 8 psychiatic disorders and cerebral organoid scMultiomics.
+Schematic plot was visualized in Adobe Illustrator.
